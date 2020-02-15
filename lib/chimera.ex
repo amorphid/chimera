18cbd064634ba57ed552206598c482a09d48d3c5
Michael Pope <mpope.cr@gmail.com>
2020-02-15 14:12:46 +0000

    Create initial commit

---
+++ lib/chimera.ex
@@ -0,0 +1,18 @@
+defmodule Chimera do
+  @moduledoc """
+  Documentation for `Chimera`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Chimera.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
